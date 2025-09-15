76c6dc743d406784155ef45721a40e6b94ed70a0
Stephen J. Lu <Stephen@StephenJLu.com>
2025-09-15 14:00:35 -0700

    scrolling dev

--- app/routes/privacy/privacy.tsx
+++ app/routes/privacy/privacy.tsx
@@ -24,7 +24,7 @@ export const Privacy = () => {
         <h1>Privacy Policy</h1>
         <p className={styles.lastUpdated}>Last updated: September 12, 2025</p>        
         <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
-<p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p>
+        <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p>
         
         <section className={styles.section}>
           <h2>Interpretation and Definitions</h2>
