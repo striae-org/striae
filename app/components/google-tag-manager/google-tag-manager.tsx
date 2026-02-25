0f0185ddac63b14602d9cc9a103b87ac2e58c8fc
Stephen J. Lu <Stephen@StephenJLu.com>
2026-02-25 13:30:03 -0700

    Revert "GTM dev"
    This reverts commit 19ef5ad95c98fca523b92ce38265d68bc86c0203.

--- app/components/google-tag-manager/google-tag-manager.tsx
+++ app/components/google-tag-manager/google-tag-manager.tsx
@@ -1,15 +1,17 @@
 interface GoogleTagManagerProps {
   id: string;
+  dataLayerName?: string;
 }
 
-const getScript = (id: string) => `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${id}');`;
+const getScript = (id: string, dataLayerName: string) =>
+  `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':` +
+  `new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],` +
+  `j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=` +
+  `'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);` +
+  `})(window,document,'script','${dataLayerName}','${id}');`;
 
-export const GoogleTagManagerHead = ({ id }: GoogleTagManagerProps) => (
-  <script data-cfasync="false" dangerouslySetInnerHTML={{ __html: getScript(id) }} />
+export const GoogleTagManagerHead = ({ id, dataLayerName = 'dataLayer' }: GoogleTagManagerProps) => (
+  <script dangerouslySetInnerHTML={{ __html: getScript(id, dataLayerName) }} />
 );
 
 export const GoogleTagManagerNoScript = ({ id }: Pick<GoogleTagManagerProps, 'id'>) => (
