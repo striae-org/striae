9be65870258a636b5ddd1891112e91d1f8c17d8d
Stephen J. Lu <Stephen@StephenJLu.com>
2026-03-14 18:07:14 -0700

    reinstate manifest

--- app/root.tsx
+++ app/root.tsx
@@ -30,6 +30,7 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: 'manifest', href: '/manifest.json' },
   { rel: 'icon', href: '/favicon.ico' },
   { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' },
 ];
