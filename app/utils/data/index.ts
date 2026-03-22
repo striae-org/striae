21834bd61736af5ed76fa146cd991bcd5660ca0a
Stephen J. Lu <Stephen@StephenJLu.com>
2026-03-21 20:15:35 -0700

    code review

--- app/utils/data/index.ts
+++ app/utils/data/index.ts
@@ -2,11 +2,11 @@ export * from './operations';
 export * from './permissions';
 
 export {
-	getConfirmationSummaryTelemetry,
-	resetConfirmationSummaryTelemetry,
-	type CaseConfirmationSummary,
-	type ConfirmationSummaryEnsureOptions,
-	type ConfirmationSummaryTelemetry,
-	type FileConfirmationSummary,
-	type UserConfirmationSummaryDocument
+  getConfirmationSummaryTelemetry,
+  resetConfirmationSummaryTelemetry,
+  type CaseConfirmationSummary,
+  type ConfirmationSummaryEnsureOptions,
+  type ConfirmationSummaryTelemetry,
+  type FileConfirmationSummary,
+  type UserConfirmationSummaryDocument
 } from './confirmation-summary/summary-core';
